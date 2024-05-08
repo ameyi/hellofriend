64cabafa8942264b2cf30121db22801e38e2e6e7
ameyi <114318190+ameyi@users.noreply.github.com>
2024-05-08 15:15:38 -0700

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+function home(){
+    window.location.replace("index.html")
+}
+function alpha(){
+    window.location.replace("alpha.html")
+}
+function second(){
+    window.location.replace("second.html")
+}
+function regular(){
+    window.location.replace("calculator.html")
+}
